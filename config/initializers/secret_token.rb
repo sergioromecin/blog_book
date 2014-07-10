5e7150575307a53c5954c968ddea655313f8fe5b
Sergio Fernando Romecin Garcia <sromecin@gmail.com>
2014-07-10 00:54:26 -0400

    Demo micropost and users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogBook::Application.config.secret_token = '230648d347dc682cd55bbfffa3ba06d2a2646169191cfaba21dc761b0b90eeb04c61d26180acfc68a78f8f53f586b6d7951a164d12593351eea2b162635b38bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BlogBook::Application.config.session_store :cookie_store, key: '_blog_book_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BlogBook::Application.config.session_store :active_record_store
